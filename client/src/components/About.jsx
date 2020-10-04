c2d42c23e019fbdb6e5da16f8aa99c6ae07f49d6
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-04 17:20:42 -0400

    Add metalfini about page

---
+++ client/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <p>Everything about Metalfini!</p>
+    </>
+  );
+};
+
+export default About;
--- client/src/routes/Index.jsx
+++ client/src/routes/Index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Home from '../components/Home';
+import About from '../components/About';
 import FourOhFour from '../components/FourOhFour';
 
 function Routes({ children }) {
@@ -12,6 +13,7 @@ function Routes({ children }) {
       <Main>
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/about' exact component={About} />
           <Route path='/*' exact component={FourOhFour} />
         </Switch>
       </Main>
