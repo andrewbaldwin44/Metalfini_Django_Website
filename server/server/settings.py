86b95912c9a805638da3fc7e17354badbefb6f15
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-04 19:20:44 -0400

    Proxy requests from front-end to back-end

--- server/server/settings.py
+++ server/server/settings.py
@@ -40,13 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
     'metalfini',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,7 +123,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ORIGIN_WHITELIST = (
-     'localhost:3000/'
- )
