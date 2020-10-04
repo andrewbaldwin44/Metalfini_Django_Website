f7cdd04970eebb21465aa7ec8c2a732514d965d3
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-04 16:37:28 -0400

    Add Global Styles with CSS Reset

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Routes from '../routes/Index';
 
+import GlobalStyles from './GlobalStyles';
+
 function App() {
   return (
     <>
+      <GlobalStyles />
       {Routes}
     </>
   )
