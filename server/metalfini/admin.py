2c3e68e386e00ab3549bcbfc9846e62f30ca8474
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-10-04 22:06:37 -0400

    Add image field to products data

--- server/metalfini/admin.py
+++ server/metalfini/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'description')
+    list_display = ('name', 'price', 'description', 'image')
 
 admin.site.register(Product, ProductAdmin)
--- server/metalfini/models.py
+++ server/metalfini/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=220)
     price = models.FloatField()
     description = models.TextField()
+    image = models.URLField(default='https://firebasestorage.googleapis.com/v0/b/metalfini.appspot.com/o/metal.jpeg?alt=media&token=6eabb394-f737-4845-8d1c-857a7e927bf3')
 
     def _str_(self):
         return self.name
